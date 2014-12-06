2281a42b2b9b2d2a84ef61dfe2f335b3937712af
shunyi <M157q.tw@gmail.com>
2014-12-07 01:04:12 +0800

    add headers for solving http://www.bomb01.com/ forbidden

--- gettitle.py
+++ gettitle.py
@@ -9,6 +9,15 @@ ptt = "www.ptt.cc/bbs"
 url  = str(sys.argv[1])
 br = mechanize.Browser()
 br.set_handle_robots(False)
+br.addheaders = [
+    ('User-agent', "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:33.0) Gecko/20100101 Firefox/33.0"),
+    ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+    ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'),
+    ('Accept-Encoding', 'none'),
+    ('Accept-Language', 'en-US,en;q=0.8'),
+    ('Connection', 'keep-alive')
+]
+
 r = br.open(url)
 
 if ptt in url and any(br.forms()):
