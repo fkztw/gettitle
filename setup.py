82cedb363981c3ee552e104f42a74ae628ad4278
shunyi <M157q.tw@gmail.com>
2015-11-18 14:15:59 +0800

    Add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+requires = [
+    'beautifulsoup4',
+    'dryscrape',
+    'requests',
+    'robobrowser',
+]
+
+setup(
+    packages=find_packages(exclude=['gettitle.bin']),
+    scripts=['gettitle/bin/gettitle'],
+    install_requires=requires,
+    name='gettitle',
+    version='0.0.1',
+    author='Shun-Yi Jheng',
+    author_email='M157q.tw@gmail.com',
+    url="https://github.com/M157q/gettitle",
+    keywords="cli, webpage, title",
+    description="Get webpage title(s) by url(s)."
+                "Print out to stdout in normal or markdown format.",
+    platforms=['Linux'],
+    license='MIT',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Utilities",
+    ],
+)
